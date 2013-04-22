537d02dfc7c9aa1ae86f1c523cf87413936382c0
Jonathan J Hunt <j@me.net.nz>
2013-04-22 00:15:07 -0700

    Seems to be reentrant - but I don't know enough to test it properly.

--- mutex/mutex.erl
+++ mutex/mutex.erl
@@ -10,6 +10,8 @@ lock() ->
 	receive
 		go ->
 			ok
+		after
+			1000 -> timeout
 	end.
 
 unlock() ->
@@ -22,17 +24,35 @@ init() ->
 
 unlocked() ->
 	receive
+		% Ignore any unlock messages
+		{unlock, _} ->
+			unlocked();
 		{lock, Pid} ->
-			locked(Pid)
+			locked(Pid, 0)
 	end.
 
-locked(Pid) ->
+locked(Pid, 0) ->
 	% Link up in case the process crashes
 	link(Pid),
 	Pid ! go,
 	receive
 		{unlock, Pid} ->
+			unlink(Pid),
 			unlocked();
 		{'EXIT', Pid, _} ->
-			unlocked()
+			unlink(Pid),
+			unlocked();
+		{lock, Pid} ->
+			locked(Pid, 1)
+	end;
+locked(Pid, N) ->
+	% We're already linked
+	Pid ! go,
+	receive
+		{unlock, Pid} ->
+			locked(Pid, N - 1);
+		{'EXIT', Pid, _} ->
+			locked(Pid, N - 1);
+		{lock, Pid} ->
+			locked(Pid, N + 1)
 	end.
